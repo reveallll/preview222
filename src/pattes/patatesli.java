4e34b1e89a685322a07fe3e775e86db64ebc2492
Serhat Diyaroğlu <serhatd1917@gmail.com>
2023-12-06 16:30:40 +0300

    first commit

---
+++ src/pattes/patatesli.java
@@ -0,0 +1,10 @@
+package pattes;
+
+public class patatesli {
+    public static void main(String[] args){
+
+
+
+        System.out.println("Patika.Dev Fullstack Java Spring boot & React Bootcamp");
+    }
+}
